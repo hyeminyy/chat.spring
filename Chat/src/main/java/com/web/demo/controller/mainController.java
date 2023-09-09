ddab986bfe01b92e39f5578c463f30d9ce8fb9c6
View <View@DESKTOP-1ELQDVG>
2023-09-10 02:04:44 +0900

    chat.springboot

---
+++ Chat/src/main/java/com/web/demo/controller/mainController.java
@@ -0,0 +1,18 @@
+package com.web.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class mainController {
+
+		@RequestMapping("/chat")
+		public ModelAndView chat() {
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("chat");
+			
+			return mv;
+			
+		}
+}
